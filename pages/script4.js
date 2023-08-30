26ade7e9591c9b0e0d46e973a6f9d83af0567f72
Cyrille CARRE <cyrille.carre@gmail.com>
2023-08-30 14:17:34 +0200

    jeu terminé

---
+++ pages/script4.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var premierChiffre = document.getElementById("premierChiffre");
+    var operateur = document.getElementById("operateur");
+    var deuxiemeChiffre = document.getElementById("deuxiemeChiffre");
+    var resultatInput = document.getElementById("resultat");
+    var validationButton = document.getElementById("validation");
+    var startButton = document.getElementById("start");
+    var acceuil = document.getElementById("boutonA")
+
+    var premier = 0;
+    var deuxieme = 0;
+    var operateurSymbole = "*"; // Déclarer operateurSymbole en tant que variable globale
+
+    startButton.addEventListener("click", function () {
+        premier = Math.floor(Math.random() * 100) + 1;
+        deuxieme = Math.floor(Math.random() * 100) + 1;
+
+        if (operateurSymbole = "*") {
+        premier = Math.floor(Math.random() * 5) + 1;
+        deuxieme = Math.floor(Math.random() * 5) + 1;
+        }
+
+        premierChiffre.textContent = premier;
+        operateur.textContent = operateurSymbole;
+        deuxiemeChiffre.textContent = deuxieme;
+    });
+
+    validationButton.addEventListener("click", function() {
+        var resultatAttendu = premier * deuxieme;
+
+        if (parseInt(resultatInput.value) === resultatAttendu) {
+            document.body.style.backgroundColor = "green";
+            console.log(resultatAttendu);
+            resultatInput.value = "";
+            setTimeout(function () {
+                document.body.style.backgroundColor = "inherit";
+                startButton.click();
+            }, 1500);
+        } else { 
+            document.body.style.backgroundColor = "red";
+            setTimeout(function () {
+                console.log(resultatAttendu);
+                resultatInput.value = "";
+                document.body.style.backgroundColor = "inherit";
+            }, 1500);
+        }
+    });
+
+    resultatInput.addEventListener("keyup", function(event) {
+        if (event.key === "Enter") {
+            validationButton.click();
+        }
+    });
+
+
+    acceuil.addEventListener("click", function() {
+        window.location.href = "/index.html"
+    })
+});
--- script.js
+++ script.js
@@ -1,60 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var premierChiffre = document.getElementById("premierChiffre");
-    var operateur = document.getElementById("operateur");
-    var deuxiemeChiffre = document.getElementById("deuxiemeChiffre");
-    var resultatInput = document.getElementById("resultat");
-    var validationButton = document.getElementById("validation");
-    var startButton = document.getElementById("start");
+document.addEventListener("DOMContentLoaded", function() {
+    var addition = document.getElementById("boutonAddition");
+    var soustraction = document.getElementById("boutonSoustraction");
+    var multiplication = document.getElementById("boutonMultiplication");
+    var laTotalChantal = document.getElementById("boutonLaTotal");
 
-    var premier = 0;
-    var deuxieme = 0;
-    var operateurSymbole = "+"; // Déclarer operateurSymbole en tant que variable globale
-
-    startButton.addEventListener("click", function () {
-        premier = Math.floor(Math.random() * 100) + 1;
-        deuxieme = Math.floor(Math.random() * 100) + 1;
-        var operateurIndex = Math.floor(Math.random() * 3);
-
-        if (operateurIndex === 0) {
-            operateurSymbole = "+";
-        } else if (operateurIndex === 1) {
-            operateurSymbole = "-";
-        } else {
-            operateurSymbole = "*";
-            premier = Math.floor(Math.random() * 5) + 1;
-            deuxieme = Math.floor(Math.random() * 5) + 1;
-        }
-
-        premierChiffre.textContent = premier;
-        operateur.textContent = operateurSymbole;
-        deuxiemeChiffre.textContent = deuxieme;
+    laTotalChantal.addEventListener("click", function() {
+        window.location.href = "pages/page1.html"
     });
 
-    validationButton.addEventListener("click", function() {
-        var resultatAttendu;
-        if (operateurSymbole === "+") {
-            resultatAttendu = premier + deuxieme;
-        } else if (operateurSymbole === "-") {
-            resultatAttendu = premier - deuxieme;
-        } else {
-            resultatAttendu = premier * deuxieme;
-        }
+    addition.addEventListener("click", function() {
+        window.location.href = "pages/page2.html"
+    });
 
-        if (parseInt(resultatInput.value) === resultatAttendu) {
-            document.body.style.backgroundColor = "green";
-            console.log(resultatAttendu);
-            resultatInput.value = "";
-            setTimeout(function () {
-                document.body.style.backgroundColor = "lightgrey";
-                startButton.click();
-            }, 1500);
-        } else { 
-            document.body.style.backgroundColor = "red";
-            setTimeout(function () {
-                console.log(resultatAttendu);
-                resultatInput.value = "";
-                document.body.style.backgroundColor = "lightgrey";
-            }, 1500);
-        }
+    soustraction.addEventListener("click", function() {
+        window.location.href = "pages/page3.html"
+    });
+
+    multiplication.addEventListener("click", function() {
+        window.location.href = "pages/page4.html"
     });
 });
